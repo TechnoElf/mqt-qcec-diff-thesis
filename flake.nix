99c126a9f28cf1e586a885c14b7ddb59dc3e0939
Janis Heims <janis.heims@tum.de>
2024-07-12 15:53:49 +0200

    misc: fix flake

--- flake.nix
+++ flake.nix
@@ -2,11 +2,43 @@
   inputs = {
     nixpkgs.url = github:nixos/nixpkgs/nixos-24.05;
     flake-utils.url = github:numtide/flake-utils;
+    typix = {
+      url = github:loqusion/typix;
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    typst-packages = {
+      url = github:typst/packages;
+      flake = false;
+    };
   };
 
-  outputs = { self, nixpkgs, flake-utils, ... }: flake-utils.lib.eachDefaultSystem (system:
+  outputs = { self, nixpkgs, flake-utils, typix, typst-packages, ... }: flake-utils.lib.eachDefaultSystem (system:
     let
       pkgs = nixpkgs.legacyPackages.${system};
+      inherit (pkgs) lib;
+
+      tx = typix.lib.${system};
+      typstPackagesSrc = "${typst-packages}/packages";
+      typstPackagesCache = pkgs.stdenv.mkDerivation {
+        name = "typst-packages-cache";
+        src = typstPackagesSrc;
+        dontBuild = true;
+        installPhase = ''
+          mkdir -p "$out/typst/packages"
+          cp -LR --reflink=auto --no-preserve=mode -t "$out/typst/packages" "$src"/*
+        '';
+      };
+
+      typstProject = {
+        typstSource = "main.typ";
+        fontPaths = [
+          "./template/resources/"
+        ];
+      };
+      typstProjectSrc = {
+        src = ./.;
+        XDG_CACHE_HOME = typstPackagesCache;
+      };
     in {
       devShell = pkgs.mkShell {
         buildInputs = with pkgs; [
@@ -14,25 +46,11 @@
         ];
       };
 
-      packages.mqt-qcec-diff-thesis = pkgs.stdenv.mkDerivation rec {
-        name = "mqt-qcec-diff-thesis";
-        src = ./.;
-
-        buildInputs = with pkgs; [
-          typst
-        ];
-
-        buildPhase = ''
-          typst compile --font-path template/resources/ main.typ
-        '';
-
-        installPhase = ''
-          mkdir -p $out
-          cp main.pdf $out/${name}.pdf
-        '';
-      };
-
+      packages.mqt-qcec-diff-thesis = tx.buildTypstProject (typstProject // typstProjectSrc);
       packages.default = self.packages.${system}.mqt-qcec-diff-thesis;
+
+      apps.watch = flake-utils.lib.mkApp { drv = tx.watchTypstProject typstProject; };
+      apps.default = self.apps.${system}.watch;
     }
   );
 }
